0fa2ef561d3b39a1af55d6a12c3ce039a593dc8c
vikuman <bansalvikas2626@gmail.com>
2018-03-15 19:35:27 +0530

    First commit

---
+++ home.php
@@ -0,0 +1,219 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Indian Institute of Tchnology</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <style>
+        .img1 
+            {
+            position: absolute;
+            z-index: -1;
+            }
+            .robotics
+            {
+                background-color:#cb9b67; 
+                margin-right:2%;
+                margin-top:2%;
+                margin-left:5%;
+                margin-bottom:2%;
+                height:270px;
+                width:230px;
+                border-radius:3px;
+            }
+            .button1{
+                width:35%;
+                margin-top:12%;
+                margin-left:55%;
+                position:absolute;
+                background-color:#cb9b67;
+                
+            }
+            
+            .team{
+            position: relative;
+             /*background-color:#cb9b67; 
+                margin-right:5%;
+                margin-top:2%;
+                margin-left:5%;
+                margin-bottom:2%;
+                height:auto;
+                width:40%;
+               padding-top:2%;
+                padding-bottom:2%;
+                border-radius:3px;  */ 
+            }
+            
+            
+           /* overlay*/
+.image {
+  display: block;
+  z-index: 1;
+  height: auto;
+  width: 100%;
+  margin-top:2.5%;
+  margin-bottom:2.5%;
+   
+}
+
+.overlay {
+  position: absolute;
+  z-index: 1;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: auto;
+  width: 95%;
+  opacity: 0;
+  transition: .5s ease;
+  background-color: #008CBA;
+  border-radius:4px; 
+  margin-bottom:2.3%;
+  margin-top:2.3%;
+  margin-left:2.5%;
+  
+}
+
+.team:hover .overlay {
+  opacity: 1;
+}
+
+            .team:hover .view{
+                opacity: 1;
+            }
+
+.text {
+  color: white;
+  font-size: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  text-align: center;
+}
+ </style>
+    </head>
+    
+    <body class="container">
+<div class="row"><div class="col-sm-12"><h3>STAB IIT Goa</h3></div></div>       
+<nav class="navbar navbar-inverse" style="background-color:#663300;width:height%;">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">STAB, IIT GOA</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">About <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">About Page1</a></li>
+            <li><a href="#">About Page2</a></li>
+            <li><a href="#">About Page3</a></li>
+          </ul>
+        </li>
+          
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Clubs <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#clubs">Clubs Page1</a></li>
+            <li><a href="#">Clubs Page2</a></li>
+            <li><a href="#">Clubs Page3</a></li>
+          </ul>
+        </li>  
+          
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Team <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Team Page1</a></li>
+            <li><a href="#">Team Page2</a></li>
+            <li><a href="#">Team Page3</a></li>
+          </ul>
+        </li> 
+          
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Contact us <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">contact Page1</a></li>
+            <li><a href="#">contact Page2</a></li>
+            <li><a href="#">contact Page3</a></li>
+          </ul>
+        </li>  
+          
+      </ul>
+      <!--<ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>-->
+    </div>
+  </div>
+</nav>
+        
+<div class="row" id="wall-paper" style="margin-bottom:3%;">
+    <div class="col-sm-12">
+     <img src="../img/clubs/cultural/img1.JPG" alt="wall paper" style="width:97.2%;z-index:-1;" class="img-rounded img1">
+     </div>
+     <h1 style="color:white;padding:15% 14% 15% 16%;"><b><i>"IDEATE, FORMULATE, CREAT, INNOVATE"</i></b></h1>
+</div>
+ 
+<p id="clubs"></p>
+<h3><b>CLUBS</b></h3> <!--start club-->
+<div style="background-color:#261a0d;margin-bottom:5%;width:100%;height:100%;margin-left:0.2%;border-radius:3px;">
+    <div class="row">
+    <div class="col-sm-4 robotics">
+      <div style="color:#724e27;margin-top:30%;"><h2><b><center>Robotics <br>And <br>Electronics Club</center></b></h2></div>
+    </div>
+    <div class="col-sm-8" >
+        <div class="col-sm-12"><p style="color:white;margin-top:10%;">dfhgfdh jhgjuytikjyu fjytuik djhtfrijh hsafd76e ergiuer iyfoerygoer gare6d5wr oiuf fjshdf ruywer jgfoier fghfg dtrytu xgf qwreaed htuhyth dfhgfdh </p></div>
+        <div class="col-sm-12"><a href=""><button class="btn button1"><span style="color:#724e27;"><b>Visit Page</b></span></button></a></div>
+    </div>
+    </div>
+    
+    <div class="row">
+    <div class="col-sm-4 robotics">
+      <div style="color:#724e27;margin-top:30%;"><h2><b><center>Web And <br>Coding Club</center></b></h2></div>
+    </div>
+    <div class="col-sm-8" >
+        <div class="col-sm-12"><p style="color:white;margin-top:10%;">dfhgfdh jhgjuytikjyu fjytuik djhtfrijh hsafd76e ergiuer iyfoerygoer gare6d5wr oiuf fjshdf ruywer jgfoier fghfg dtrytu xgf qwreaed htuhyth dfhgfdh </p></div>
+        <div class="col-sm-12"><a href=""><button class="btn button1"><span style="color:#724e27;"><b>Visit Page</b></span></button></a></div>
+    </div>
+    </div>
+</div><!--end start club-->
+
+        
+<p id="teams"></p>
+<h3><b>TEAMS</b></h3> <!--start teams-->
+<div class="row" style="background-color:#261a0d;margin-bottom:5%;width:100%;height:100%;margin-left:0.2%;border-radius:3px;">
+   
+    
+   
+    <div class="col-sm-6 team">
+      <img src="../img/clubs/cultural/img1.JPG" alt="" class="image">
+        <div class="overlay">
+            <a href="#"><div class="text" style="color:white;"><h2><b><center>ITSP-2017</center></b></h2></div></a>
+        </div>
+    </div>
+    
+    <div class="col-sm-6 team">
+        <img src="../img/clubs/sports/iitmintersport.jpg" alt="" class="image img-rounded">
+        <div class="overlay">
+            <div class="text" style="color:white;"><h2><b><center>IIT Goa<br>Motor Sports</center></b></h2></div>
+            <div class="view" style="margin-left:32%;"><a href="#"><button class="btn button" style="color:#724e27;">view page</button></a></div>
+        </div>
+    </div>
+    
+</div><!--end teams-->   
+        
+      
+        
+</body>
+</html>
+
